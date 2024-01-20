49f70113d122d7c62a623f56e473c76855c55709
Nurjon <nurjon2477@gmail.com>
2024-01-20 21:14:58 +0500

    mode-toggle edit

--- components/shared/dashboard.tsx
+++ components/shared/dashboard.tsx
@@ -1,7 +1,5 @@
 import Image from "next/image";
 import Github from "@/public/dashboards/github.png";
-import Dashboardd from "@/public/dashboards/dashboard.png";
-import Netlify from "@/public/dashboards/netlify.png";
 import Link from "next/link";
 
 const Dashboard = () => {
@@ -43,8 +41,37 @@ const Dashboard = () => {
             </p>
           </div>
         </div>
-        
-        <div className=" flex flex-col md:flex-row md:my-8 my-2 justify-center md:justify-start md:gap-12">
+          {/* <div className=" flex flex-col md:flex-row justify-center md:my-4 md:justify-start md:gap-12">
+            <Image
+              src={Dashboardd}
+              alt="Rectangle"
+              className=" w-full md:w-[400px]"
+              width={400}
+              height={200}
+            />
+
+            <div>
+              <h1 className="text-3xl font-['Heebo'] text-center md:text-start font-bold text-[#21243d] dark:text-white/70 py-5 ">
+                Github Dashboard
+              </h1>
+              <div className=" flex gap-5 justify-center md:justify-start py-2">
+                <div className="bg-[#142850] flex items-center justify-center w-16 h-8  rounded-lg">
+                  <span className="text-lg font-['Heebo'] font-black leading-[60px] text-white ">
+                    2023
+                  </span>
+                </div>
+                <span className="text-xl font-['Heebo'] text-[#8695a4] dark:text-white/70 ">
+                  Dashboard
+                </span>
+              </div>
+              <p className="font-['Heebo'] text-[#21243d] dark:text-white/70 w-full text-center md:text-start md:w-2/3 py-4">
+                Amet minim mollit non deserunt ullamco est sit aliqua dolor do
+                amet sint. Velit officia consequat duis enim velit mollit.
+                Exercitation veniam consequat sunt nostrud amet.
+              </p>
+            </div>
+          </div> */}
+        {/* <div className=" flex flex-col md:flex-row md:my-8 my-2 justify-center md:justify-start md:gap-12">
           <Image
             src={Netlify}
             alt="Rectangle"
@@ -53,7 +80,7 @@ const Dashboard = () => {
             height={200}
           />
 
-          {/* <div>
+          <div>
             <h1 className="text-3xl font-['Heebo'] text-center md:text-start font-bold text-[#21243d] dark:text-white/70 py-5 ">
               <Link
                 href={"https://app.netlify.com/teams/nurjonerkinov7/overview"}
@@ -76,8 +103,8 @@ const Dashboard = () => {
               amet sint. Velit officia consequat duis enim velit mollit.
               Exercitation veniam consequat sunt nostrud amet.
             </p>
-          </div> */}
-        </div>
+          </div>
+        </div> */}
       </div>
     </div>
   );
--- components/shared/mode-toggle.tsx
+++ components/shared/mode-toggle.tsx
@@ -7,34 +7,52 @@ import { useTheme } from "next-themes";
 import { Button } from "@/components/ui/button";
 import {
   DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
+  
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 
 export function ModeToggle() {
-  const { setTheme } = useTheme();
+  const { theme, setTheme } = useTheme();
+
+  const toggleTheme = () => {
+    if (theme === "light") {
+      setTheme("dark");
+    } else {
+      setTheme("light");
+    }
+  };
 
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant="outline" size="icon">
-          <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+          {theme === "light" ? (
+            <>
+              <MoonIcon
+                onClick={toggleTheme}
+                className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+              />
+              <MoonIcon
+                onClick={toggleTheme}
+                className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+              />
+            </>
+          ) : (
+            <>
+              <SunIcon
+                onClick={toggleTheme}
+                className="h-[1.2rem] w-[1.2rem] rotate-0 scale-0 transition-all dark:-rotate-90 dark:scale-100"
+              />
+              <MoonIcon
+                onClick={toggleTheme}
+                className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-100 transition-all dark:rotate-0 dark:scale-0"
+              />
+            </>
+          )}
+
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-        <DropdownMenuItem onClick={() => setTheme("light")}>
-          Light
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("dark")}>
-          Dark
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("system")}>
-          System
-        </DropdownMenuItem>
-      </DropdownMenuContent>
     </DropdownMenu>
   );
 }
